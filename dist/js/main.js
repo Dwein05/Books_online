951e5d64d0375fda1d4b9e7b0dbab9947c99a4b5
Daria <dwein05@gmail.com>
2019-04-14 20:28:41 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _user = _interopRequireDefault(require("./user"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var hello = function hello(name) {
+  return "hello ".concat(name);
+};
+
+console.log(hello(_user["default"]));
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiaGVsbG8iLCJuYW1lIiwiY29uc29sZSIsImxvZyIsInVzZXIiXSwibWFwcGluZ3MiOiI7O0FBQUE7Ozs7QUFFQSxJQUFNQSxLQUFLLEdBQUcsU0FBUkEsS0FBUSxDQUFDQyxJQUFELEVBQVU7QUFBRSx5QkFBZ0JBLElBQWhCO0FBQXlCLENBQW5EOztBQUVBQyxPQUFPLENBQUNDLEdBQVIsQ0FBWUgsS0FBSyxDQUFDSSxnQkFBRCxDQUFqQiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB1c2VyIGZyb20gJy4vdXNlcidcclxuXHJcbmNvbnN0IGhlbGxvID0gKG5hbWUpID0+IHsgcmV0dXJuIGBoZWxsbyAke25hbWV9YDsgfTtcclxuXHJcbmNvbnNvbGUubG9nKGhlbGxvKHVzZXIpKSJdLCJmaWxlIjoibWFpbi5qcyJ9
